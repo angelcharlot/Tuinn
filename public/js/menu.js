c5db22d316b9eab257e532a3c50360d187b3dba8
angel charlot <angelcm18205@gmail.com>
2022-11-17 14:32:37 +0100

    primir comit

---
+++ public/js/menu.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    var imagen_logo = document.getElementById("imagen_logo");
+    var imagen_fondo = document.getElementById("todo");
+    imagen_fondo.style.backgroundImage ="url("+imagen_logo.src+")";
+   
+   
+    
+
+    
+};
