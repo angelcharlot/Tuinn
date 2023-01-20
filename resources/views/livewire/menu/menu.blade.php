38b29cbeaa07ca408da2f6555e5d5080c8f74730
angel charlot <angelcm18205@gmail.com>
2023-01-20 19:16:01 +0100

    otros cambios

--- resources/views/livewire/menu/menu.blade.php
+++ resources/views/livewire/menu/menu.blade.php
@@ -1,12 +1,12 @@
-<div class=" bg-teal-300">
+<div class="bg-teal-300 ">
     {{-- loading --}}
-    <div wire:loading wire:target="idioma,producto" class="fixed z-40 w-full h-full top-0 left-0 bg-gray-800 bg-opacity-75">
-        <div class="w-ful h-full ">
+    <div wire:loading wire:target="idioma,producto" class="fixed top-0 left-0 z-40 w-full h-full bg-gray-800 bg-opacity-75">
+        <div class="h-full w-ful ">
             <div class="flex justify-center h-full">
 
-                <div class="w-24 h-24 my-auto z-50 ">
+                <div class="z-50 w-24 h-24 my-auto ">
                     <div role="status">
-                        <svg class="animate-spin -ml-1 mr-3 h-18 w-18 text-blue-800" xmlns="http://www.w3.org/2000/svg"
+                        <svg class="mr-3 -ml-1 text-blue-800 animate-spin h-18 w-18" xmlns="http://www.w3.org/2000/svg"
                             fill="none" viewBox="0 0 24 24">
                             <circle class="opacity-25" cx="12" cy="12" r="10"
                                 stroke="currentColor" stroke-width="4"></circle>
@@ -22,25 +22,25 @@
         </div>
     </div>
 
-    <div  wire:ignore.self class=" bg-local pb-28 mt-2 container mx-auto rounded-md shadow-sm ">
+    <div  wire:ignore.self class="container mx-auto mt-2 bg-local rounded-md shadow-sm pb-28">
         {{-- DATOS DEL NEGOCIO --}}
-        <div class="  w-full my-3 grid grid-cols-6 md:grid-cols-12 gap-1">
+        <div class="grid w-full grid-cols-6 gap-1 my-3 md:grid-cols-12">
             {{-- 1  imagen del negocio --}}
-            <div class="  col-span-2 md:col-span-1 mx-auto  flex items-center">
-                <img id="imagen_logo" class="object-scale-down  h-20 rounded-full" src="{{ asset($negocio->img) }}" alt="">
+            <div class="flex items-center col-span-2 mx-auto md:col-span-1">
+                <img id="imagen_logo" class="object-scale-down h-20 rounded-full" src="{{ asset($negocio->img) }}" alt="">
             </div>
             {{-- 2  nombre del negocio --}}
-            <div class=" col-span-4 md:col-span-9 mx-auto  ">
-                <div class="text-center font-Lobster   mt-3 text-2xl md:text-4xl text-gray-700 font-bold">
+            <div class="col-span-4 mx-auto md:col-span-9">
+                <div class="mt-3 text-2xl font-bold text-center text-gray-700 font-Lobster md:text-4xl">
                     {{ $negocio->name }}
                 </div>
             </div>
         </div>
 
-        <div class=" w-4/12  m-5">
+        <div class="w-4/12 m-5 ">
             {{-- idioma --}}
-            <label class="text-xs text-gray-500 mx-1 " for="">Idioma</label>
-            <select wire:model.debounce="idioma"   class=" rounded-lg " >
+            <label class="mx-1 text-xs text-gray-500 " for="">Idioma</label>
+            <select wire:model.debounce="idioma"   class="rounded-lg " >
                 <option value="es">español</option>
                 <option value="en">Inglés</option>
                 <option value="fr">Francés</option>
@@ -50,12 +50,12 @@
             </select>
         </div>
         <div
-            class=" border-gray-200 container w-11/12 mx-auto  border-b border-light shadow-card  py-1 px-2 sm:px-6 md:px-8 md:py-2 ">
+            class="container w-11/12 px-2 py-1 mx-auto border-b border-gray-200 border-light shadow-card sm:px-6 md:px-8 md:py-2">
             <ul class="flex items-center">
                 {{-- miga home --}}
                 <li class="flex items-center">
                     <a wire:click="nav_categorias('principal')"
-                        class="font-semibold text-2xl text-blue-800 hover:text-blue-500  cursor-pointer hover:text-primary">
+                        class="text-2xl font-semibold text-blue-800 cursor-pointer hover:text-blue-500 hover:text-primary">
                         <i class="bi bi-filter-circle"></i>
                     </a>
                     @if (count($migas) > 0)
@@ -70,7 +70,7 @@
                         <li class="flex items-center">
 
                             <a wire:click="nav_categorias('{{ $miga['id'] }}')"
-                                class="font-semibold hover:text-blue-800 cursor-pointer text-base text-black hover:text-primary">
+                                class="text-base font-semibold text-black cursor-pointer hover:text-blue-800 hover:text-primary">
                                 {{ $miga['name'] }}
                             </a>
 
@@ -83,7 +83,7 @@
                         <li class="flex items-center">
 
                             <a wire:click="nav_categorias('{{ $miga['id'] }}')"
-                                class="font-semibold text-base text-blue-800  border-b-4 border-blue-800  hover:text-primary">
+                                class="text-base font-semibold text-blue-800 border-b-4 border-blue-800 hover:text-primary">
                                 {{ $miga['name'] }}
                             </a>
                         </li>
@@ -93,22 +93,22 @@
         </div>
         {{-- selector de categorias --}}
 
-        <div class=" container w-11/12 h-10 mx-auto mt-2 grid grid-cols-4 gap-1 mb-5">
+        <div class="container grid w-11/12 h-10 grid-cols-4 gap-1 mx-auto mt-2 mb-5 ">
             @foreach ($categorias as $categoria)
                 <div wire:click="nav_categorias('{{ $categoria->id }}')"
-                    class="flex items-center cursor-pointer rounded-md  hover:bg-blue-500 text-gray-100  bg-blue-500 font-medium border-blue-600 shadow-md border-b  ">
-                    <h1 class="w-full text-center capitalize text-xs md:text-base ">{{ $categoria->name }}</h1>
+                    class="flex items-center font-medium text-gray-100 bg-blue-500 border-b border-blue-600 rounded-md shadow-md cursor-pointer hover:bg-blue-500 ">
+                    <h1 class="w-full text-xs text-center capitalize md:text-base ">{{ $categoria->name }}</h1>
 
                 </div>
             @endforeach
         </div>
 
         {{-- cartas de los productos --}}
-        <div wire:change wire:target="idioma" class="  grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-3 text-xs mx-auto">
+        <div wire:change wire:target="idioma" class="grid grid-cols-2 gap-3 mx-auto text-xs sm:grid-cols-4 md:grid-cols-6">
             @foreach ($apartados as $apartado)
             {{--condiciono a que los apartados esten en el filtro--}}
                 @if ($productos->where('descrip3', '=', $apartado->descrip3)->where('activo','=',1)->count() > 0)
-                    <div class="ml-5 text-blue-800 col-span-2 sm:col-span-4 md:col-span-6 text-left font-Lobster text-4xl">
+                    <div class="col-span-2 ml-5 text-4xl text-left text-blue-800 sm:col-span-4 md:col-span-6 font-Lobster">
                          {{ $apartado->descrip3 }}
                 </div>
                 @endif
@@ -118,9 +118,9 @@
                 @foreach ($productos->where('descrip3', '=', $apartado->descrip3)->where('activo','=',1) as $key => $producto)
                     
                     <div wire:click.prefetch="producto('{{ $producto->id }}')"
-                        class=" bg-white relative grid-cols-2 grid border border-gray-200 m-1 rounded-md shadow-xs p-1 hover:border-indigo-300 hover:shadow-2xl ">
+                        class="relative grid grid-cols-2 p-1 m-1 bg-white border border-gray-200 rounded-md shadow-xs hover:border-indigo-300 hover:shadow-2xl">
                         
-                        <div class=" absolute sombrero">
+                        <div class="absolute sombrero">
                            @if (($key  % 2) == 0)
                                <img src="{{asset('storage/banner/carnaval.png')}}" alt="">
                             @else
@@ -129,11 +129,11 @@
                             
                         </div>
                         
-                        <div class="col-span-2 text-center font-extrabold">
+                        <div class="col-span-2 font-extrabold text-center">
                             {{ $producto->name }}
                         </div>
                         <div>
-                            <img class=" h-24 object-fill rounded-md" src="{{ asset($producto->img) }}"
+                            <img class="object-fill h-24 rounded-md " src="{{ asset($producto->img) }}"
                                 alt="Sunset in the mountains">
                         </div>
                         <div>
@@ -143,7 +143,7 @@
                                     @if (isset($producto->presentaciones))
                                         @foreach ($producto->presentaciones as $presentacion)
                                             <tr>
-                                                <td class="pl-2 text-green-900 text-xs font-medium ">
+                                                <td class="pl-2 text-xs font-medium text-green-900 ">
                                                     {{ $presentacion->name }}: &euro;{{ $presentacion->precio_venta }}
                                                 </td>
 
@@ -151,7 +151,7 @@
                                         @endforeach
                                     @else
                                         <tr>
-                                            <td class="pl-2 text-green-900 text-2xl font-medium ">
+                                            <td class="pl-2 text-2xl font-medium text-green-900 ">
                                                 &euro;{{ $producto->precio_venta }}
                                             </td>
 
@@ -163,7 +163,7 @@
                             </table>
 
                         </div>
-                        <div class="col-span-2 text-ellipsis overflow-hidden h-10  ">
+                        <div class="h-10 col-span-2 overflow-hidden text-ellipsis ">
                             @if ($idioma!="es")
                             @foreach ($producto->idiomas->where('idioma','=',$idioma) as $idioma_select)
                                 {{$idioma_select->descrip}}
@@ -173,14 +173,14 @@
                             @endif
                             
                         </div>
-                        <div class="col-span-2 grid grid-cols-2">
+                        <div class="grid grid-cols-2 col-span-2">
                             <div>
                                 {{ $producto->likes->where('tipo', 1)->count() }}
-                                <i class="bi bi-heart-fill text-red-800 mx-auto"></i>
+                                <i class="mx-auto text-red-800 bi bi-heart-fill"></i>
                             </div>
                             <div>
                                 {{ $producto->likes->where('tipo', 0)->count() }}
-                                <i class="bi bi-heartbreak-fill  mx-auto "></i>
+                                <i class="mx-auto bi bi-heartbreak-fill "></i>
                             </div>
                         </div>
                         <div class="col-span-2">
@@ -189,7 +189,7 @@
                                 @foreach ($producto->categorias as $categoria)
                                     <span
                                         style="font-size: 8px;line-height: 11px;letter-spacing: 0.027em;font-weight: 875;"
-                                        class="inline-block bg-gray-200 rounded-full px-1 py-1  font-semibold text-gray-700 mr-2 mb-2">#{{ $categoria->name }}</span>
+                                        class="inline-block px-1 py-1 mb-2 mr-2 font-semibold text-gray-700 bg-gray-200 rounded-full">#{{ $categoria->name }}</span>
                                 @endforeach
                             </div>
 
@@ -205,22 +205,25 @@
     </div>
     {{-- 3  datos del negocio --}}
     <div
-        class="  grid  grid-cols-2 mx-auto border-t-2 border-indigo-300 bg-black fixed bottom-0 w-full p-2 text-white ">
+        class="fixed bottom-0 grid w-full grid-cols-3 p-2 mx-auto text-sm text-white bg-black border-t-2 border-indigo-300 ">
         <div>
-            <div class="w-full text-left text-sm font-bold">
+            <div class="w-full text-sm text-left">
                 Direccion: {{ $negocio->direccion }}
             </div>
         </div>
         <div>
-            <div class="text-left  text-xs font-bold">
+            <div class="text-xs font-bold text-left">
                 Tlf:{{ $negocio->telefono1 }}
             </div>
-            <div class="text-left mb-2 text-xs font-bold">
+            <div class="mb-2 text-xs font-bold text-left">
                 Tlf:{{ $negocio->telefono2 }}
             </div>
         </div>
-        <div class="col-span-2 mx-auto text-xs">
-            designer by Angel Charlot
+        <div>
+          <a href="whatsapp://send?text=https://tuinn.es/menu/1">Compartir <i class="bi bi-whatsapp"></i></a>  
+        </div>
+        <div class="col-span-3 mx-auto text-xs">
+            designer by ING. Angel Charlot, olvera cadiz
         </div>
 
     </div>
@@ -228,30 +231,30 @@
     <x-jet-dialog-modal wire:model.debounce="open">
         <x-slot name="title">
             <div class="grid grid-cols-6">
-                <div class=" col-span-5 "
+                <div class="col-span-5 "
                     style="font-size: 22px;line-height: 25px;letter-spacing: 0.042em;font-weight: 697;">
                     {{ $producto_selecionado->name }}
                 </div>
                 <div wire:click="$set('open',false)"
-                    class=" col-span-1 text-right text-red-800 font-bold cursor-pointer ">
+                    class="col-span-1 font-bold text-right text-red-800 cursor-pointer ">
                     <i class="bi bi-x-lg"></i>
                 </div>
             </div>
         </x-slot>
         <x-slot name="content">
-            <div class="justify-center h-36 md:h-48 w-5/6 mx-auto  ">
-                <img class=" h-full object-scale-down mx-auto " src="{{ asset($producto_selecionado->img) }}"
+            <div class="justify-center w-5/6 mx-auto h-36 md:h-48 ">
+                <img class="object-scale-down h-full mx-auto " src="{{ asset($producto_selecionado->img) }}"
                     alt="Sunset">
             </div>
             @foreach ($producto_selecionado->presentaciones as $presentacion)
-                <div class=" w-5/6 mx-auto"
+                <div class="w-5/6 mx-auto "
                     style="letter-spacing: -0.044em;font-weight: 600;font-size: 21px;font-style: italic;">
                     {{ $presentacion->name }} &euro; {{ $presentacion->precio_venta }}
                 </div>
             @endforeach
 
-            <div class=" mt-1 w-full mx-auto text-base font-bold">descripcion:</div>
-            <div class="w-full mx-auto max-h-40 p-2 overflow-auto text-base text-gray-700 text-justify">
+            <div class="w-full mx-auto mt-1 text-base font-bold ">descripcion:</div>
+            <div class="w-full p-2 mx-auto overflow-auto text-base text-justify text-gray-700 max-h-40">
 
                 @if ($idioma!="es")
                 @foreach ($producto_selecionado->idiomas->where('idioma','=',$idioma) as $idioma_select)
@@ -265,8 +268,8 @@
                
             </div>
             @if ($producto_selecionado->descrip2)
-                <div class=" mt-1 w-full mx-auto text-xs font-bold">Maridaje:</div>
-                <div class="w-full mx-auto  overflow-auto text-xs text-gray-700 text-justify">
+                <div class="w-full mx-auto mt-1 text-xs font-bold ">Maridaje:</div>
+                <div class="w-full mx-auto overflow-auto text-xs text-justify text-gray-700">
                     @if ($idioma!="es")
                     @foreach ($producto_selecionado->idiomas->where('idioma','=',$idioma) as $idioma_select)
                         {{$idioma_select->descrip2}}
@@ -281,9 +284,9 @@
                 </div>
             @endif
 
-            <div class="w-full text-left p-3 ml-5">
+            <div class="w-full p-3 ml-5 text-left">
                 @foreach ($producto_selecionado->alargenos as $alargeno)
-                    <img class="inline h-10 w-auto" src="{{ asset($alargeno->img) }}" alt="{{ $alargeno->name }}">
+                    <img class="inline w-auto h-10" src="{{ asset($alargeno->img) }}" alt="{{ $alargeno->name }}">
                 @endforeach
 
 
@@ -296,18 +299,18 @@
                     <x-jet-button wire:click="$set('open',false)">cerrar</x-jet-button>
                 </div>
 
-                <div class="w-3/4 flex justify-between p-2">
+                <div class="flex justify-between w-3/4 p-2">
 
                     <div wire:click="likes(0,'{{ $producto_selecionado->id }}')"
                         class="bg-red-200 rounded-md hover:bg-red-100">
 
-                        <i class="  p-3 bi bi-hand-thumbs-down mx-auto text-red-700 cursor-pointer text-lg"></i>
+                        <i class="p-3 mx-auto text-lg text-red-700 cursor-pointer bi bi-hand-thumbs-down"></i>
                     </div>
 
                     <div wire:click="likes(1,'{{ $producto_selecionado->id }}')"
                         class="bg-green-200 rounded-md hover:bg-green-100">
 
-                        <i class=" rounded-md p-3 bi bi-hand-thumbs-up mx-auto text-green-700 cursor-pointer text-lg">
+                        <i class="p-3 mx-auto text-lg text-green-700 rounded-md cursor-pointer bi bi-hand-thumbs-up">
                         </i>
                     </div>
                 </div>
--- resources/views/livewire/panel.blade.php
+++ resources/views/livewire/panel.blade.php
@@ -1,16 +1,17 @@
-<div class=" container mx-auto pb-10">
+<div class="w-11/12 pb-10 mx-auto ">
     
-    <div class=" grid-cols-3 grid gap-2 mt-10" >
-        <div class="p-5 h-52 border border-green-500 rounded-lg shadow-sm  ">
-            <div>nro de productos:{{$nro_productos}}</div>
+    <div class="grid grid-cols-1 gap-2 mt-10 " >
+        <div class="p-5 border border-green-900 rounded-lg shadow-sm h-68 ">
+            <div class="h-10 pt-1 mb-5 text-center text-white bg-green-500 rounded-md">PRODUCTOS</div>
+            <div class="flex" >Cantidad de productos: <h1 class="ml-5 text-center text-white bg-blue-900 rounded-full w-7 h-7">{{$nro_productos}}</h1></div>
              
              @foreach ($categorias as $categoria)
-                 <div>{{$categoria->name}}: {{$categoria->productos->count()}}</div>
+                 <div class="flex" >{{$categoria->name}}: <h1 class="ml-5 text-center text-white bg-blue-900 rounded-full w-7 h-7">{{$categoria->productos->count()}}</h1></div>
              @endforeach
-             productos fuera de la carta: {{$productos_false}}
+             <div class="flex">   productos fuera de la carta: <h1 class="ml-5 text-center text-white bg-blue-900 rounded-full w-7 h-7">{{$productos_false}}</h1></div>
         </div>
-        <div class="p-5 h-52 border border-blue-500 rounded-lg shadow-sm  "> cuadros de info </div>
-        <div class="p-5 h-52 border border-red-500 rounded-lg shadow-sm  "> cuadros de info </div>
+        <div class="p-5 border border-blue-500 rounded-lg shadow-sm h-52 "> cuadros de info </div>
+        <div class="p-5 border border-red-500 rounded-lg shadow-sm h-52 "> cuadros de info </div>
     </div>
 
 
